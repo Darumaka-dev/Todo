900e4e13ce45338b6dbb1ca1af5356d8930f704d
Elina Samoilenko <elina.samoilenko@epam.com>
2026-04-06 20:38:25 +0300

    chore: add code review suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,20 @@ import Checkbox from '@mui/material/Checkbox';
 import ButtonGroup from '@mui/material/ButtonGroup';
 // import './App.css'
 
+/** для ids использовать библиотеку uuid ("f353ca91-4fc5-49f2-9b9e-304f83d11914") */
 
 //почему если есть данные в localeStorage, то при перезагрузке страницы на секунду появляется {total: 0, completed: 0}? Из-за того, что state с задачами сперва пустой?
-const useCount = (todos, initialValue) => {
-  const [count, setCount] = useState(initialValue);
+const useTodosCount = (todos) => {
+  const [count, setCount] = useState({total: 0, completed: 0});
 
-  const getTask = () => {
-    let total = 0;
-    let completed = 0;
-    todos.map((item) => {
-      total++;
-      item.done ? completed++ : completed;
+  useEffect(() => {
+    setCount({ 
+      total: todos.length,
+      completed: todos.filter((todo) => todo.done).length
     });
-    setCount(prev =>  ({...prev, total, completed}));
-  };
+  }, [todos]);
 
-  const checkTodosList = () => {
-   return todos.length !== 0 ? getTask() : initialValue;
-  };
-
-
-  useEffect(() => {
-    checkTodosList();
-    }, []);
-
-  useEffect(() => {
-    getTask();
-    }, [todos]);
-
-  return {count}
-  
+  return count;
 }
 
 function App() {
@@ -49,65 +33,53 @@ function App() {
   // const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [filter, setFilter] = useState('all');
-  const {count} = useCount(todos, {total: 0, completed: 0})
+  const { total, completed } = useTodosCount(todos);
 
   
 
   const updateInput = (e) => {
     const data = e.target.value;
     setInputValue(data);
-    
   };
 
   const handleClickAdd = () => {
-    setTodos(prev => [...prev, {id: todos.length + 1, text: inputValue, done: false}]);
+    setTodos(prev => [...prev, {id: new Date(), text: inputValue, done: false}]);
     setInputValue('');
   };
 
   const toggleCheckbox = (id) => {
-    setTodos(todos.map(item => 
+    const checkedTodos = todos.map(item => 
         item.id == id 
           ? { ...item, done: !item.done }
           : item
       )
-    );
+
+    setTodos(checkedTodos);
   };
 
   const deleteTask = (id) => {
-    setTodos(() => {
-      return todos.filter((item) => item.id !== id)
-    });
+    setTodos(todos.filter((item) => item.id !== id));
   };
 
   const changeFilter = (filter) => {
     setFilter(filter);
   };
 
-  const getFilteredTask = (filter) => {
-    let result = '';
-    switch (filter) {
-      case 'all':
-        result = todos.map((item) => item);
-        break;
-      case 'active':
-        result = todos.filter((item) => (item.done == false));
-        break;
-      case 'completed':
-        result = todos.filter((item) => item.done == true);
-        break;
-      default:
-        result = [todos.map((item) => item)];
-    };
+  const getFilteredTask = () => {
+    /** early return */
+    if (filter === 'active') {
+      return todos.filter((item) => !item.done);
+    }
 
-    return result;
+    if (filter === 'completed') {
+      return todos.filter((item) => item.done)
+    }
+
+    return todos;
   };
 
   const resetCompletedTask = () => {
-    setTodos(prev => {
-      return prev.filter((item) => {
-        return item.done !== true
-      });
-    });
+    setTodos(todos.filter((item) => !item.done));
   };
 
   const resetAll = () => {
@@ -121,20 +93,12 @@ function App() {
   //   return dataLocaleStorage ? setTodos(JSON.parse(dataLocaleStorage)) : todos;
   // },[]);
 
-  //обновляем id когда меняется длина массива
-  useEffect(() => {
-    setTodos(todos.map((item, index) => ({...item, id: index + 1})));
-  },[todos.length]);
-
   //обновляем localStorage 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   },[todos]);
 
-
- console.log(todos)
- console.log(count)
-  const filteredTask = getFilteredTask(filter);
+  const filteredTask = getFilteredTask();
 
   return (
     <>
@@ -142,11 +106,15 @@ function App() {
         To do list
       </Typography>
 
+      {/* <Filter /> */}
+
       <ButtonGroup sx={{mb: 3}} variant="outlined" aria-label="Basic button group">
         <Button onClick={() => changeFilter('all')} sx={filter == 'all' ? {backgroundColor: 'primary.dark', color: 'white'} : {backgroundColor: 'primary.main '}}>All</Button>
         <Button onClick={() => changeFilter('active')} sx={filter == 'active' ? {backgroundColor: 'primary.dark', color: 'white'} : {backgroundColor: 'primary.main '}}>Active</Button>
         <Button onClick={() => changeFilter('completed')} sx={filter == 'completed' ? {backgroundColor: 'primary.dark', color: 'white'} : {backgroundColor: 'primary.main '}}>Completed</Button>
       </ButtonGroup>
+
+      {/* <TodoForm /> */}
 
       <Stack spacing={2} direction="row">
         <TextField id="outlined-basic" label="Task name" variant="outlined" onChange={updateInput} value={inputValue}/>
@@ -154,13 +122,15 @@ function App() {
         <Button variant="contained" onClick={resetCompletedTask}>Clear Completed</Button>
       </Stack>
       
+      {/* <TodosList /> */}
+
       <List>
         {filteredTask.length !== 0 
           ? filteredTask.map((item) => <ListItem key={item.id}><Checkbox checked={item.done} onChange={() => toggleCheckbox(item.id)}/>{item.text} <Button color="error" sx={{ml: 2, fontSize: 10}} variant="outlined" onClick={(() => deleteTask(item.id))}>delete</Button></ListItem>) 
           : <Typography variant="caption" gutterBottom sx={{ display: 'block' }}>Empty List</Typography>}
       </List>
 
-      <Typography variant="caption" gutterBottom sx={{ display: 'block' }}>{`Total task: ${count.total}, completed task: ${count.completed}`} </Typography>
+      <Typography variant="caption" gutterBottom sx={{ display: 'block' }}>{`Total task: ${total}, completed task: ${completed}`} </Typography>
       <Button variant="contained" color="error" onClick={resetAll}>Reset All</Button>
       
     </>
